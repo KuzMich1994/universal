ed64e0b496a2e25987d76df78b527b65d0e811d1
KuzMich1994 <drumer19940310@mail.ru>
2020-10-13 23:13:55 +0300

    create: news section and responsive

--- js/main.js
+++ js/main.js
@@ -4,4 +4,15 @@ $(document).ready(function () {
     document.querySelector(".header__wrapper_mobile").classList.toggle("header__wrapper_mobile_visible")
     document.querySelector("body").classList.toggle("scroll-hidden")
   });
+
 });
+
+
+let flagButton = document.querySelectorAll('.flag');
+flagButton.forEach(elem => elem.addEventListener('click', (evt) => {
+  if (evt.target.classList.contains('flag_active')) {
+    evt.target.classList.remove('flag_active')
+  } else {
+    evt.target.classList.add('flag_active')
+  }
+}))
