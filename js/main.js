376c93a1a6e4b5f30bac8b8d8cf94ed82c52ea69
KuzMich1994 <drumer19940310@mail.ru>
2020-10-17 18:47:24 +0300

    create: added up-button and compressed photo

--- js/main.js
+++ js/main.js
@@ -98,7 +98,21 @@ $(document).ready(function () {
       }
     }
   })
+
+  let upButton = document.querySelector('.up-button');
+
+  window.onscroll = function () {
+  if (window.pageYOffset > 3700) {
+    upButton.classList.add('shown');
+  } else {
+    upButton.classList.remove('shown');
+  }
+  };
   
+  upButton.onclick = function () {
+  window.scrollTo({ top: 0, behavior: 'smooth'});
+  };
+
 });
 
 
